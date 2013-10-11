60e88c0a697d575ef0c5fab54d18f009f65cda1f
Sahil Parmar <spparmar@compute-3-4.local>
2013-10-11 02:43:21 -0400

    Modify code to update CR3, which allows address assignment from pmpl_table and not a hard code value

--- sys/paging.c
+++ sys/paging.c
@@ -7,9 +7,9 @@
 #define ENTRIES_PER_PTE  512
 #define ENTRIES_PER_PDE  512
 #define ENTRIES_PER_PDPE 512
-#define ENTRIES_PER_PML  512
+#define ENTRIES_PER_PML4 512
 
-static uint64_t *pml_table;
+static uint64_t *pml4_table;
 static uint64_t *pdpe_table;
 static uint64_t *pde_table;
 static uint64_t *pte_table;
@@ -18,18 +18,18 @@ void init_paging()
 {
     int i, phys_addr; 
 
-    pml_table  = (uint64_t*) pmmngr_alloc_block();
+    pml4_table = (uint64_t*) pmmngr_alloc_block();
     pdpe_table = (uint64_t*) pmmngr_alloc_block();
     pde_table  = (uint64_t*) pmmngr_alloc_block();
     
 
     // Mappings for virtual address range [0xFFFFFFFF80200000, 0xFFFFFFFF80600000]
     // to physical address range [0x200000, 0x600000]
-    pml_table[511]  = (uint64_t) pdpe_table | 0x3;
+    pml4_table[511] = (uint64_t) pdpe_table | 0x3;
     pdpe_table[510] = (uint64_t) pde_table  | 0x3;
 
     pte_table = (uint64_t*) pmmngr_alloc_block();
-    pde_table[1] = (uint64_t) pte_table  | 0x3; 
+    pde_table[1] = (uint64_t) pte_table | 0x3; 
     
     // Assigning physical addresses 0x200000 to 0x400000 in PTE
     phys_addr = 0x200000;
@@ -44,7 +44,7 @@ void init_paging()
     
     // Assigning physical addresses 0x400000 to 0x600000 in PTE
     phys_addr = 0x400000;
-    for(i = 0; i< ENTRIES_PER_PTE; ++i)
+    for(i = 0; i < ENTRIES_PER_PTE; ++i)
     {
         pte_table[i] = phys_addr | 0x3;
         phys_addr += 0x1000;
@@ -54,10 +54,7 @@ void init_paging()
     pte_table[184] = 0xB8000 | 0x3;
 
     // Set CR3 register to address of PML4 table
-    __asm__(
-        "movq $0x400000, %rax;"
-        "movq %rax, %cr3;"
-    );
+    asm volatile ("movq %0, %%cr3;" :: "r"((uint64_t)pml4_table));
 
     // Change Video base address
     init_screen(0xFFFFFFFF804B8000);
