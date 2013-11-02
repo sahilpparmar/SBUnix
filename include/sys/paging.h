abebb4dc41e34f29536d78499c922b16663cb502
Sohil Gandhi <sogandhi@sbrocks.cewit.stonybrook.edu>
2013-11-02 17:14:55 -0400

    Self Referencing Page Tables

--- include/sys/paging.h
+++ include/sys/paging.h
@@ -1,6 +1,7 @@
-
 void init_paging(uint64_t, uint64_t, uint64_t);
 void map_virt_phys_addr(uint64_t,uint64_t,uint64_t);
+void map(uint64_t,uint64_t,uint64_t);
+void self_ref(uint64_t,uint64_t);
 
 uint64_t* alloc_pte(uint64_t *pde_table,int pde_off);
 uint64_t* alloc_pde(uint64_t *pdpe_table,int pdpe_off);
@@ -9,4 +10,3 @@ uint64_t* alloc_pdpe(uint64_t *pml4_table,int pml4_off);
 void set_pml4_t(uint64_t *addr);
 uint64_t* get_pml4_t();
 uint64_t* get_ker_pml4_t();
-
