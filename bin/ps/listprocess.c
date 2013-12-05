4f4d077f858099f29903185186b85a5459536521
Sahil Parmar <spparmar@compute-3-5.local>
2013-12-05 03:28:32 -0500

    Fixed support for BG processes, cat on Disk files, ls on files

--- bin/ps/listprocess.c
+++ bin/ps/listprocess.c
@@ -3,13 +3,7 @@
 
 int main(int argc, char* argv[])
 {
-    printf("Process %d (parent %d) is ", getpid(), getppid());
-
-    for (int i = 0; i < argc; i++)
-        printf("%s ", argv[i]);
-
     listprocess();
-
     return 0;
 }
 
